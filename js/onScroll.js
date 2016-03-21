f65d70b1283cac22432927db3270fd981f26e41e
Kikoplot <quesnel.jeremie@gmail.com>
2016-03-21 17:06:42 +0100

    Init commit

---
+++ js/onScroll.js
@@ -0,0 +1,31 @@
+$("document").ready(function($){
+    var nav = $('#anchor-article');
+    var nav2 = $('#anchor-interview');
+    var nav3 = $('#anchor-contact');
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 970 && $(this).scrollTop() < 2800) {
+            nav.addClass("fixed");
+        }
+        else {
+          if ($(this).scrollTop() >= 3030) {
+              nav2.addClass("fixed");
+          }
+          else {
+            if ($(this).scrollTop() < 3030) {
+              nav2.removeClass("fixed");
+            }
+          }
+          if ($(this).scrollTop() >= 5030) {
+            nav3.addClass("fixed");
+          }
+          else {
+            if($(this).scrollTop() <= 970){
+              nav.removeClass("fixed");
+            }
+          }
+          if($(this).scrollTop() < 5030){
+            nav3.removeClass("fixed");
+          }
+      }
+    });
+});
---
+++ js/resize.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+  // $(window).resize() est appelée chaque fois que la fenêtre est redimensionnée par l'utilisateur.
+  $(window).resize(function() {
+    $("#monbloc").css({
+      position:'absolute',
+      left:($(window).width() - $("#monbloc").outerWidth()) / 2,
+      top:($(window).height() - $("#monbloc").outerHeight()) / 2
+    });
+    $("#info1").css({
+      left:($(window).width() - $("#info1").outerWidth()) / 100,
+      top:($(window).height() - $("#info1").outerHeight()) / 1.85
+    });
+    $("#info2").css({
+      left:($(window).width() - $("#info2").outerWidth()) / 2.5,
+      top:($(window).height() - $("#info2").outerHeight()) / 1.55
+    });
+    $("#info3").css({
+      left:($(window).width() - $("#info3").outerWidth()) / 1.2,
+      top:($(window).height() - $("#info3").outerHeight()) / 4
+    });
+    $("#menu").css({
+      top:($(window).height() - $("#info3").outerHeight()) / 2.1
+    });
+  });
+});
+
+$(window).load(function() {
+  // au chargement complet de la page, la fonction resize() est appelée une fois pour initialiser le centrage.
+  $(window).resize();
+});
