acbabd2f35e789ed10972d01ca33ad2212910db0
Kikoplot <quesnel.jeremie@gmail.com>
2016-05-02 23:23:29 +0200

    Final push

--- js/onScroll.js
+++ js/onScroll.js
@@ -3,30 +3,33 @@ $("document").ready(function($){
     var fix = $('#ancrearticle');
     var nav2 = $('#anchor-interview');
     var nav3 = $('#anchor-contact');
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 970 && $(this).scrollTop() < 2800) {
-            nav.addClass("fixed");
+      $(window).scroll(function () {
+        if(window.innerWidth >= 1900) {
+
+          if ($(this).scrollTop() > 970 && $(this).scrollTop() < 2800) {
+              nav.addClass("fixed");
+          }
+          else {
+            if ($(this).scrollTop() >= 3030) {
+                nav2.addClass("fixed");
+            }
+            else {
+              if ($(this).scrollTop() < 3030) {
+                nav2.removeClass("fixed");
+              }
+            }
+            if ($(this).scrollTop() >= 5030) {
+              nav3.addClass("fixed");
+            }
+            else {
+              if($(this).scrollTop() <= 970){
+                nav.removeClass("fixed");
+              }
+            }
+            if($(this).scrollTop() < 5030){
+              nav3.removeClass("fixed");
+            }
         }
-        else {
-          if ($(this).scrollTop() >= 3030) {
-              nav2.addClass("fixed");
-          }
-          else {
-            if ($(this).scrollTop() < 3030) {
-              nav2.removeClass("fixed");
-            }
-          }
-          if ($(this).scrollTop() >= 5030) {
-            nav3.addClass("fixed");
-          }
-          else {
-            if($(this).scrollTop() <= 970){
-              nav.removeClass("fixed");
-            }
-          }
-          if($(this).scrollTop() < 5030){
-            nav3.removeClass("fixed");
-          }
-      }
-    });
+        }
+      });
 });
